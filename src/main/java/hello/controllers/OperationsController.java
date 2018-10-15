3967fe8ae51cf6cd270cb97703f49203229910b9
Dorian Wilhelm <dorian.wilhelm@pm.me>
2018-10-15 23:46:15 +0200

    ajoute des fonctionnalitees demandee dans le tp

--- src/main/java/hello/controllers/OperationsController.java
+++ src/main/java/hello/controllers/OperationsController.java
@@ -1,20 +1,44 @@
 package hello.controllers;
 
 import hello.services.OperationsServices;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import javax.script.ScriptException;
 import java.math.BigDecimal;
+
 
 @RestController
 public class OperationsController {
 
+    private final OperationsServices ops = new OperationsServices();
+
+    // TODO faire les tests pour toutes ses routes
+    // TODO Gestion des erreurs quand l'utilisateur envoie des mauvaises erreurs
+
+    @RequestMapping(value = "/add/{nbOne}/{nbTwo}")
+    public Integer add(OperationsServices ops, @PathVariable Integer nbOne, @PathVariable Integer nbTwo){
+        return ops.add(nbOne, nbTwo);
+    }
+
+    @RequestMapping("/subtraction/{nbOne}/{nbTwo}")
+    public Integer subtraction(OperationsServices ops, @PathVariable Integer nbOne, @PathVariable Integer nbTwo){
+        return ops.subtraction(nbOne, nbTwo);
+    }
+
+    @RequestMapping("/multiply/{nbOne}/{nbTwo}")
+    public Integer multiply(OperationsServices ops, @PathVariable Integer nbOne, @PathVariable Integer nbTwo){
+        return ops.multiply(nbOne, nbTwo);
+    }
+
+    @RequestMapping("/divise/{nbOne}/{nbTwo}")
+    public Integer divise(OperationsServices ops, @PathVariable Integer nbOne, @PathVariable Integer nbTwo){
+        return ops.divise(nbOne, nbTwo);
+    }
 
     @RequestMapping("/eval")
     public BigDecimal eval(@RequestParam(value="operation") String operation) {
-        OperationsServices ops = new OperationsServices();
+        // TODO Ajouter les test pour la route eval
+        // TODO Faire en sorte de retourner une erreur quand un retour utilisateur bug
+        operation = operation.replace(" ", "+");
         return ops.evaluation(operation);
     }
 
--- src/main/java/hello/services/OperationsServices.java
+++ src/main/java/hello/services/OperationsServices.java
@@ -8,17 +8,29 @@ import java.math.BigDecimal;
 @Service
 public class OperationsServices {
 
-    public OperationsServices() {
+    public OperationsServices() {}
 
+    // TODO faire les tests pour toutes les methodes
+
+    public Integer add(Integer numberOne, Integer numberTwo){
+        return numberOne + numberTwo;
     }
 
+    public Integer subtraction(Integer numberOne, Integer numberTwo){
+        return numberOne - numberTwo;
+    }
 
-    // + dans l'url = %2B
+    public Integer multiply(Integer numberOne, Integer numberTwo){
+        return numberOne * numberTwo;
+    }
+
+    public Integer divise(Integer numberOne, Integer numberTwo){
+        return numberOne / numberTwo;
+    }
+
     public BigDecimal evaluation(String operations) {
         Expression expression = new Expression(operations);
-        BigDecimal result = expression.eval();
-        System.out.println(result);
-        return result;
+        return expression.eval();
     }
 
 
